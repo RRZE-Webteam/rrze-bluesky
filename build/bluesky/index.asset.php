be25bc0ee17132bb3f1633a871312203409f4f25
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-01-16 16:30:50 +0100

    feat(base functionality) - Basic block public timeline
    #6 Add bsky logo to posts embedded from bsky

--- build/bluesky/index.asset.php
+++ build/bluesky/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9fd8d801bb99722f2071');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9bd050dde1c939707492');
