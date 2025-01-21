10c382ea4af4b342367edb8d596d2fedd32f1372
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-01-21 14:23:44 +0100

    feat(base functionality) - basic block post php render
    Unterstützung von Videos, falls Videoplugin aktiviert ist. #6, #9, #30

--- includes/Render.php
+++ includes/Render.php
@@ -17,6 +17,8 @@ class Render
     public static function enqueueStyle()
     {
         wp_enqueue_style('rrze-bluesky');
+        wp_enqueue_script('rrze-video-front-js');
+        wp_enqueue_style('rrze-video-plyr');
     }
 
     /**
@@ -36,7 +38,7 @@ class Render
 
         $isPublicTimeline = !empty($args['publicTimeline']);
         $uri             = isset($args['uri']) ? trim($args['uri']) : '';
-        $uri =  'https://bsky.app/profile/visdoms.bsky.social/post/3lgablq3pok2m';
+        $uri =  '';
         $limit           = isset($args['limit']) ? (int) $args['limit'] : 10;
 
         // If publicTimeline is set, show timeline
@@ -203,10 +205,14 @@ class Render
             // Typically, you might have something like "playlist" or "thumbnail" in the embed
             $mediaUrl = isset($embed['playlist'])  ? $embed['playlist']  : '';
             $poster   = isset($embed['thumbnail']) ? $embed['thumbnail'] : '';
+            $videoCid = isset($embed['cid']) ? $embed['cid'] : '';
+            $videoHeight = isset($embed['height']) ? $embed['height'] : '';
+            $videoWidth = isset($embed['width']) ? $embed['width'] : '';
+            $videoAspectRatioClass = $this->get_aspectratio_class($videoHeight, $videoWidth);
 
             $html .= '<div class="bsky-video">';
             // Render the Vidstack or your custom video player:
-            $html .= $this->renderVidstackVideo($mediaUrl, $poster);
+            $html .= $this->renderVidstackVideo($mediaUrl, $poster, $videoCid, $videoAspectRatioClass);
             $html .= '</div>';
         }
 
@@ -233,43 +239,47 @@ class Render
 
         $html .= '    </div>';
         if (!$hideFooter) {
-        $html .= '    <hr />';
-        $html .= '    <div class="bsky-stat-section">';
-        $html .= '      <div class="bsky-stat-icons">';
+            $html .= '    <hr />';
+            $html .= '    <div class="bsky-stat-section">';
+            $html .= '      <div class="bsky-stat-icons">';
 
-        // Like info
-        $html .= '        <div class="bsky-like-info">';
-        $html .= '          <svg class="bsky-like-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">';
-        $html .= '            <path fill="currentColor" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />';
-        $html .= '          </svg> ' . (int)$likeCount;
-        $html .= '        </div>';
+            // Like info
+            $html .= '        <div class="bsky-like-info">';
+            $html .= '          <svg class="bsky-like-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">';
+            $html .= '            <path fill="currentColor" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />';
+            $html .= '          </svg> ' . (int)$likeCount;
+            $html .= '        </div>';
 
-        // Retweet / Repost info
-        $html .= '        <div class="bsky-retweet-info">';
-        $html .= '          <svg class="bsky-retweet-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">';
-        $html .= '            <path fill="currentColor" d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32l-112 0c-17.7 0-32-14.3-32-32l0-128 32 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l32 0 0 128c0 53 43 96 96 96l112 0zM304 96c-17.7 0-32 14.3-32 32s14.3 32 32 32l112 0c17.7 0 32 14.3 32 32l0 128-32 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-32 0 0-128c0-53-43-96-96-96L304 96z" />';
-        $html .= '          </svg> ' . (int)$repostCount;
-        $html .= '        </div>';
+            // Retweet / Repost info
+            $html .= '        <div class="bsky-retweet-info">';
+            $html .= '          <svg class="bsky-retweet-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">';
+            $html .= '            <path fill="currentColor" d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32l-112 0c-17.7 0-32-14.3-32-32l0-128 32 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l32 0 0 128c0 53 43 96 96 96l112 0zM304 96c-17.7 0-32 14.3-32 32s14.3 32 32 32l112 0c17.7 0 32 14.3 32 32l0 128-32 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-32 0 0-128c0-53-43-96-96-96L304 96z" />';
+            $html .= '          </svg> ' . (int)$repostCount;
+            $html .= '        </div>';
 
-        // Comment / Reply
-        $html .= '        <div class="bsky-comment-info">';
-        $html .= '          <a href="' . esc_url($this->getPostUrl($handle, $uri)) . '">';
-        $html .= '            <svg class="bsky-reply-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">';
-        $html .= '              <path fill="currentColor" d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />';
-        $html .= '            </svg>';
-        $html .= '          </a>';
-        $html .= '        </div>';
+            // Comment / Reply
+            $html .= '        <div class="bsky-comment-info">';
+            $html .= '          <a href="' . esc_url($this->getPostUrl($handle, $uri)) . '">';
+            $html .= '            <svg class="bsky-reply-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">';
+            $html .= '              <path fill="currentColor" d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />';
+            $html .= '            </svg>';
+            $html .= '          </a>';
+            $html .= '        </div>';
 
-        $html .= '      </div>';
+            $html .= '      </div>';
 
-        // "Read X replies on Bluesky"
-        $html .= '      <div class="bsky-reply">';
-        $html .= '        <a href="' . esc_url($this->getPostUrl($handle, $uri)) . '" class="bsky-reply-count">';
-        $html .= '          ' . esc_html__('Read', 'rrze-bluesky') . ' ' . (int)$replyCount . ' ' . esc_html__('replies on Bluesky', 'rrze-bluesky');
-        $html .= '        </a>';
-        $html .= '      </div>';
+            // "Read X replies on Bluesky"
+            $html .= '      <div class="bsky-reply">';
+            $html .= '        <a href="' . esc_url($this->getPostUrl($handle, $uri)) . '" class="bsky-reply-count">';
+            if ($replyCount > 0) {
+                $html .= '          ' . esc_html__('Read', 'rrze-bluesky') . ' ' . (int)$replyCount . ' ' . esc_html__('replies on Bluesky', 'rrze-bluesky');
+            } else {
+                $html .= '          ' . esc_html__('Read on Bluesky', 'rrze-bluesky');
+            }
+            $html .= '        </a>';
+            $html .= '      </div>';
 
-        $html .= '    </div>';
+            $html .= '    </div>';
         }
         $html .= '  </footer>';
         $html .= '</article></div>';
@@ -331,25 +341,28 @@ class Render
      * @param string $poster   Poster/thumbnail image
      * @return string HTML for the video element/player
      */
-    public function renderVidstackVideo($videoUrl, $poster = '')
+    public function renderVidstackVideo($videoUrl, $poster = '', $videoId = '', $aspectRatioClass='ar-9-16')
     {
-        //TODO: Fix the function... Rethink the Vidstack embedding
-        $title       = 'Video Embed';
         $aspectRatio = '9/16';
 
         ob_start();
 ?>
-        <div class="vidstack-player" style="--aspect-ratio: <?php echo esc_attr($aspectRatio); ?>;">
-            <video
+        <div
+            class="rrze-video-container"
+            data-video-id="<?php echo esc_attr($videoId); ?>"
+            style="--aspect-ratio: <?php echo esc_attr($aspectRatio); ?>;">
+            <media-player
+                class="<?php echo esc_attr($aspectRatioClass); ?>"
                 src="<?php echo esc_url($videoUrl); ?>"
-                poster="<?php echo esc_url($poster); ?>"
-                controls>
-                <?php echo esc_html($title); ?>
-            </video>
+                poster="<?php echo esc_url($poster); ?>">
+                <media-provider></media-provider>
+                <media-video-layout></media-video-layout>
+            </media-player>
         </div>
 <?php
         return ob_get_clean();
     }
+
 
     /**
      * Build a Bluesky profile link from the handle.
@@ -411,4 +424,43 @@ class Render
             . $this->renderPost($embeddedPost, true)
             . '</div>';
     }
+
+     /**
+     * Retrieves the appropriate aspect ratio class for FAU video embeds.
+     * 
+     * This function determines the correct CSS class to apply based on 
+     * the aspect ratio provided in the arguments. If no aspect ratio or an 
+     * unrecognized aspect ratio is provided, it defaults to 'ar-16-9'.
+     *
+     * Available aspect ratios and their corresponding CSS classes:
+     * - 4/3     -> ar-4-3
+     * - 21/9    -> ar-21-9
+     * - 1/1     -> ar-1-1
+     * - 2.35/1  -> ar-234-1
+     * - 2.40/1  -> ar-240-1
+     * - 9/16    -> ar-9-16
+     * 
+     * @param array $arguments Associative array with the 'aspectratio' key potentially set to a string representing the desired aspect ratio.
+     * @return string Returns the corresponding CSS class string based on the provided aspect ratio.
+     * @since 3.5.1
+     */
+    public static function get_aspectratio_class($height, $width)
+    {
+        // convert height to number
+        $height = (int) $height;
+        $width = (int) $width;
+
+        // Check for division by zero
+        if ($width === 0) {
+            return 'ar-9-16'; // default aspect ratio
+        }
+
+        $aspect_ratio = $height / $width;
+        if ($aspect_ratio > 1) {
+            return 'ar-9-16';
+        } else {
+            return 'ar-9-16';
+        }
+    }
+
 }
